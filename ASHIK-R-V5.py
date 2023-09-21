44449df3ddb74f9e98ac83d22a406b4f8781cdc0
MD ASHIK CHOWDHURY <140885475+RX-ASHIK@users.noreply.github.com>
2023-09-21 17:44:11 +0600

    Update and rename ASHIK-R-V5.py to ASHIK-R-6.py

--- ASHIK-R-V5.py
+++
@@ -1,5 +0,0 @@
-# Developer : ASHIK CHOWDHURY 
-# FB LINK : https://www.facebook.com/ashik.khan444
-# Alhamdulillah for everything 👉❤️👈
-# -------------------------------
-print(f"{warna} 6v came for 6:00 pm @everyone")
