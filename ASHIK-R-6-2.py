79a8d080907c3b94aaaa4b9d7ff318001867d703
MD ASHIK CHOWDHURY <140885475+RX-ASHIK@users.noreply.github.com>
2023-09-29 21:21:13 +0600

    Update ASHIK-R-6-2.py

--- ASHIK-R-6-2.py
+++ ASHIK-R-6-2.py
@@ -4,4 +4,4 @@
 # Alhamdulillah for everything 👉❤️👈
 # WhatsApp : 01935091078
 # -------------------------------
-print(f"\033[1;91m[\033[1;92m√\033[1;91m] \033[92;1\x1b[1;97m𝚈𝙾𝚄𝚁 𝚃𝙾𝙾𝙻𝚂 𝙰𝙲𝚃𝙸𝚅𝙴 NEW UPDATE CAMPAIGN SOON NEXT DAY [𝙿𝚁𝙴𝙼𝙸𝚄𝙼] ")
+print(f"\033[1;91m[\033[1;92m√\033[1;91m] \033[92;1\x1b[1;97m𝚈𝙾𝚄𝚁 𝚃𝙾𝙾𝙻𝚂 𝙰𝙲𝚃𝙸𝚅𝙴 NOW  NEW UPDATE CAMING SOON NEXT DAY [𝙿𝚁𝙴𝙼𝙸𝚄𝙼] FOR APPROVAL AND PASSWORD  ")
