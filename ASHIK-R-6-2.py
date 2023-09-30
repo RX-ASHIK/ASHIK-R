0285146a637b14bb20abd264ff85c3b0978eae18
MD ASHIK CHOWDHURY <140885475+RX-ASHIK@users.noreply.github.com>
2023-09-30 11:00:11 +0600

    Update and rename ASHIK-R-6-2.py to ASHIK-R-6-3.py

--- ASHIK-R-6-2.py
+++
@@ -1,7 +0,0 @@
-# Developer : ASHIK CHOWDHURY 
-# FB LINK : https://www.facebook.com/ashik.khan444
-# VERSION 6.2V
-# Alhamdulillah for everything 👉❤️👈
-# WhatsApp : 01935091078
-# -------------------------------
-print(f"\033[1;91m[\033[1;92m√\033[1;91m] \033[92;1\x1b[1;97m𝚈𝙾𝚄𝚁 𝚃𝙾𝙾𝙻𝚂 𝙰𝙲𝚃𝙸𝚅𝙴 NOW  NEW UPDATE CAMING SOON NEXT DAY [𝙿𝚁𝙴𝙼𝙸𝚄𝙼] FOR APPROVAL AND PASSWORD  ")
