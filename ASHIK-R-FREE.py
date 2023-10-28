bb84ce4fa2d8df9e7c4f514f79c74a09451d380f
MD ASHIK CHOWDHURY <140885475+RX-ASHIK@users.noreply.github.com>
2023-10-28 20:18:14 +0600

    Update and rename ASHIK-R-FREE.py to ASHIK-R-9.0.py

--- ASHIK-R-FREE.py
+++
@@ -1,8 +0,0 @@
-# Developer : ASHIK CHOWDHURY 
-# FB LINK : https://www.facebook.com/ashik.khan444
-# Alhamdulillah for everything 👉❤️👈
-# -------------------------------
-
-print("\033[1;39m[\033[1;32m●\033[1;39m] \033[1;32m15 DAYS 250TAK \033[0;92m")
-print("\033[1;39m[\033[1;32m+\033[1;39m] \033[1;32m15 DAYS 250TAK \033[0;92m")
-print("\033[1;39m[\033[1;32m●\033[1;39m] \033[1;32mCOURCE + COMAND 30 DAYS 1000 TAKA \033[0;92m")
